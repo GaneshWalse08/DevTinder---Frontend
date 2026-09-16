7a9ac5f5e03bcd659133f0356d1f022bd3216b89
Ganesh Walse <ganeshwalse1975@gmail.com>
2026-09-16 16:51:48 +0530

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename="/">
+
+      <Routes >
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />}/>
+        </Route>
+      </Routes>
+
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
